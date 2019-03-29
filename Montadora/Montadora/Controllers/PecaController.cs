48e10ec49895209996cb232a3a0c00504f587e9e
Alan Silva <alanmdo2008@hotmail.com>
2019-03-29 12:34:20 -0400

    [TSK] Peça
    [NEW] Adicionado Action Index Add Update no Controller Peca

--- Montadora/Montadora/Controllers/PecaController.cs
+++ Montadora/Montadora/Controllers/PecaController.cs
@@ -9,6 +9,19 @@ namespace Montadora.Controllers
 {
     public class PecaController : Controller
     {
-        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
     }
 }
